a079e00c81fa197d04584173f1069ece20087fd2
Yura Pataridze <yurapatarr19@yandex.ru>
2026-03-22 22:29:54 +0300

    deleted data.json and logic of 1st entering cuz user can add any site

--- main.py
+++ main.py
@@ -4,14 +4,7 @@ import json
 import sys
 
 current_ver = 'v1.0.0'
-json_data_file_name = 'data.json'
 hosts = 'C:\Windows\System32\drivers\etc\hosts'
-
-def isFirstTime(json_file = json_data_file_name):
-    with open(json_file) as f:
-        data = json.load(f)
-
-    return data.get('howManyTimesOpened')
 
 def changeHosts(siteURL):
     try:
@@ -19,34 +12,24 @@ def changeHosts(siteURL):
             h.write(f'\n127.0.0.1 {siteURL}')
         return f"Congcongratulation! You've just blocked {siteURL}!"
     except Exception as e:
-        return 'Some ERROR: ' + e
+        return f'Oops! ERROR(changeHosts): {e}'
 
 def main():
-    if isFirstTime() == 0:
-        print(f"Hello, It's TUNGUSKA {current_ver}")
-        action = input('Input an action below:\n1 - Input site URL I want to block\n')
-        if action == '1':
-            siteURL = input('Input a site URL (e.g., 1xbet.ru)')
-            answer = input(f"Are You seriously think it's good idea to block {siteURL}? [Y/n]")
-            if answer == 'Y':
-                changeHosts(siteURL)
-                input('Press any button to leave')
-                sys.exit(0)
-            elif answer == 'n':
-                input(f'Press any button to leave and then start the TUNGUSKA {current_ver} again')
-                sys.exit(0)
-            else:
-                sys.exit(1)
+    print(f"Hello, It's TUNGUSKA {current_ver}")
+    action = input('Input an action below:\n1 - Input site URL I want to block\n')
+    if action == '1':
+        siteURL = input('Input a site URL (e.g., 1xbet.ru)')
+        answer = input(f"Are You seriously think it's good idea to block {siteURL}? Really, read each letter to check if it's exactly site URL You wanted to block!!!\n [Y/n]")
+        if answer == 'Y':
+            print(changeHosts(siteURL))
+            input('Press ENTER button to leave')
+            sys.exit(0)
+        elif answer == 'n':
+            input(f'Press ENTER button to leave and then start the TUNGUSKA {current_ver} again')
+            sys.exit(0)
         else:
             sys.exit(1)
-
-    else: 
-        print(f"Hello, It's TUNGUSKA {current_ver}")
-        action = input('Input an action below:\n1 - Turn off\n')
-
-        if action == '1':
-            pass
-        else:
-            sys.exit(1)
+    else:
+      sys.exit(1)
 
 main()
